345d1e9a62514e25815d3576d8b265c01f373a3a
Aybüke FIRAT <66526972+aybukefirat@users.noreply.github.com>
2021-02-15 22:00:53 +0300

    Add files via upload

---
+++ DictHomeWork/MyDictionary.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DictHomeWork
+{
+    class MyDictionary<T,K>
+    {
+        T[] ItemKey;
+        K[] ItemValue;
+        public MyDictionary()
+        {
+            ItemKey = new T[0];
+            ItemValue = new K[0];
+
+        }
+
+        public void Add(T key, K value)
+        {
+            T[] tempArrayK = ItemKey;
+            K[] tempArrayV = ItemValue;
+
+            ItemKey = new T[ItemKey.Length + 1];
+            ItemValue = new K[ItemValue.Length + 1];
+
+            for (int i = 0; i < tempArrayK.Length; i++)
+            {
+                ItemKey[i] = tempArrayK[i];
+                ItemValue[i] = tempArrayV[i];
+
+            }
+
+            ItemKey[ItemKey.Length - 1] = key;
+            ItemValue[ItemValue.Length - 1] = value;
+
+        }
+    }
+}
---
+++ DictHomeWork/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DictHomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> InfoDict = new Dictionary<int, string>();
+
+            InfoDict.Add(1, "Aybuke Fırat");
+            InfoDict.Add(2, "Cansu Tutar");
+            InfoDict.Add(3, "Doğa Keskin");
+
+            foreach(var infodict in InfoDict)
+            {
+                Console.WriteLine(InfoDict);
+            }
+            
+        }   
+    }
+}
